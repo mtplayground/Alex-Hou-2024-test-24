22aff7d1c23f8e459686819c6953222a6ba6d5c1
corust-ci <ci@corust.ai>
2026-05-10 00:07:10 +0000

    feat: Dockerfile, Nginx config, and .env.example

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE?: string;
+  readonly VITE_BASE_PATH?: string;
+  readonly VITE_ENABLE_SOUND?: "true" | "false";
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
